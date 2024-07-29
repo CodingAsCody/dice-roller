3f52090aa47ea7ea77a0866ce4074ec9d41864f3
CodingAsCody <CodingAsCody@proton.me>
2024-07-28 21:17:48 -0700

    converted .glb to jsx component, added cannon physics package, set two dice

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import { useState, useRef } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
-import { OrbitControls, Environment } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-const diceFaceRotations = [
-    [0, 0, 0],           // Face 1
-    [0, Math.PI / 2, 0], // Face 2
-    [Math.PI / 2, 0, 0], // Face 3
-    [-Math.PI / 2, 0, 0],// Face 4
-    [0, -Math.PI / 2, 0],// Face 5
-    [Math.PI, 0, 0]      // Face 6
-];
-
-const Model = () => {
-    const diceRef = useRef();
-    const gltf = useLoader(GLTFLoader, 'Dice.glb');
-
-    const handleDiceClick = () => {
-        if (diceRef.current) {
-            // Randomly select a rotation that corresponds to a dice face
-            const [x, y, z] = diceFaceRotations[Math.floor(Math.random() * diceFaceRotations.length)];
-            diceRef.current.rotation.set(x, y, z);
-        }
-    }
-    return (
-        <>
-            <primitive 
-                object={gltf.scene} 
-                ref={diceRef}
-                position={[0,0,0]}
-                onClick={handleDiceClick}
-            />
-        </>
-    )
-}
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Environment, CameraControls } from '@react-three/drei';
+import { Model } from './components/Model';
 
 export default function App() {
     return (
-        <div>
-            <Canvas>
-                <Model />
-                <OrbitControls />
-                <Environment preset="forest" background />
+        <div style={{ width: "80vw", height: "50vh" }}>
+            <Canvas shadows camera={{position:[2.25,3,0.25],fov:120}}>
+                <Model position={[0, 0.5, 1.5]}/>
+                <Model position={[0, 0.5, -1.5]} />
+                <CameraControls />
+                <Environment preset="city" background />
             </Canvas>
         </div>
     )
---
+++ src/components/Model.js
@@ -0,0 +1,37 @@
+import React, { useState, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF('/Dice.glb')
+  const mesh = useRef();
+  const [rotate, setRotate] = useState(false);
+
+  useFrame(({clock}) => {
+    if (rotate) {
+      const a = clock.getElapsedTime() * 4.0;
+      mesh.current.rotation.x = a;
+      mesh.current.rotation.z = -a;
+    }
+  });
+
+  const handleClick = () => {
+    if (rotate) setRotate(false);
+    else setRotate(true);
+  }
+
+  return (
+    <group {...props} dispose={null}>
+      <mesh 
+        castShadow 
+        receiveShadow 
+        geometry={nodes.dice.geometry} 
+        material={materials.White} 
+        ref={mesh} 
+        onClick={handleClick}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload('/Dice.glb')
